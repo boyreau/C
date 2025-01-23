f5ab6f19f6a9c776d2d92feb4b4283472d6415c7
Arthur <bnzlvosnb@mozmail.com>
2025-01-23 13:52:40 +0100

    some prints but not enough

--- test/common.c
+++ test/common.c
@@ -6,7 +6,7 @@
 /*   By: aboyreau <bnzlvosnb@mozmail.com>                     +**+ -- ##+     */
 /*                                                            # *   *. #*     */
 /*   Created: 2024/12/23 17:46:01 by aboyreau          **+*+  * -_._-   #+    */
-/*   Updated: 2024/12/23 17:59:29 by aboyreau          +#-.-*  +         *    */
+/*   Updated: 2024/12/23 18:17:00 by aboyreau          +#-.-*  +         *    */
 /*                                                     *-.. *   ++       #    */
 /* ************************************************************************** */
 
@@ -19,9 +19,20 @@
 #include <csetjmp>
 #include <signal.h>
 
+#define ABRT "Aborted (core dumped)\n"
+#define SEGF "Segmentation fault (core dumped)\n"
+
 _Noreturn static void abort_goto(int _)
 {
-	(void) _;
+	switch (_)
+	{
+		case SIGABRT:
+			write(1, ABTR, strlen(ABRT));
+			break;
+		case SIGSEGV:
+			write(1, SEGF, strlen(SEGF));
+			break;
+	}
 	longjmp(env, i + 1);
 }
 
@@ -33,6 +44,7 @@ int main(void)
 {
 #ifdef COVERAGE
 	signal(SIGABRT, abort_goto);
+	signal(SIGSEGV, abort_goto);
 #endif
 #ifdef COVERAGE
 	for (test_nb = 0; test_nb < ft_tablen(tests); test_nb = setjmp(env))
